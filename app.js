50cf1ff12afecab5d7d902ae633c0726d56e1245
Sobhit Kumar <sobhit2010@gmail.com>
2018-03-12 00:35:44 +0530

    Express setup and Routes and db connection

--- app.js
+++ app.js
@@ -4,10 +4,52 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database');
 
+//connect to database
+mongoose.connect(config.database);
+
+//on connection
+mongoose.connection.on('connected', () => {
+    console.log('connected to database '+ config.database);
+})
+
+//on error
+mongoose.connection.on('error', (err) => {
+    console.log('database connection error'+ err);
+})
+
+/**
+ * initailize app with express
+ */
 const app= express();
 
+const users = require('./routes/users');
+
+// port number
 const port= 3000;
+
+// cors middleware
+app.use(cors());
+
+//set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// body-parser middleware
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
+/**
+ * index route
+ */
+app.get('/', (req, res) => {
+    res.send('Invalid Endpoint');
+});
+
+/**
+ * Start server 
+ */
 app.listen(port, () => {
-    console.log('Server started at port '+port);
+    console.log('Server started at port '+ port);
 });
