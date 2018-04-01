790e2ecce47fe2f379ddf0fe8e243eb87621c386
Sobhit Kumar <sobhit2010@gmail.com>
2018-04-01 23:41:43 +0530

    Add code for profile route to fetch user details

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -30,6 +30,20 @@ export class AuthService {
     return this.http.post('http://localhost:3000/users/authenticate', user, {headers:headers})
     .map(res => res.json());
   }
+
+  getProfile(){
+    let headers = new Headers();
+    this.loadToken();
+    headers.append('Authorization', this.authToken);
+    headers.append('content-Type', 'application/json');
+
+    /***
+     * Observable here
+     */
+    return this.http.get('http://localhost:3000/users/profile', {headers:headers})
+    .map(res => res.json());
+  }
+
   // store user data to local storage
   storeUserData(token, user){
     localStorage.setItem('id_token', token);
@@ -37,6 +51,11 @@ export class AuthService {
     
     this.authToken = token;
     this.user = user;
+  }
+
+  loadToken(){
+    const token= localStorage.getItem('id_token');
+    this.authToken = token;
   }
 
   logout(){
